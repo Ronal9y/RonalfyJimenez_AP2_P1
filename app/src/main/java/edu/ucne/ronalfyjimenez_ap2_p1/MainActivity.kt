0cb982af0278ad96dfb49091a15e615e14fc22a2
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-01 21:13:00 -0400

    se agrego la nav al main

--- app/src/main/java/edu/ucne/ronalfyjimenez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/ronalfyjimenez_ap2_p1/MainActivity.kt
@@ -11,37 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.ronalfyjimenez_ap2_p1.presentation.navigation.HuacalesNavHost
 import edu.ucne.ronalfyjimenez_ap2_p1.ui.theme.RonalfyJimenez_AP2_P1Theme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RonalfyJimenez_AP2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                HuacalesNavHost(nav = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RonalfyJimenez_AP2_P1Theme {
-        Greeting("Android")
     }
 }
