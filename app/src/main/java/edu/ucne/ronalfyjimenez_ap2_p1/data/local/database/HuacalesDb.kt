9665eb20ef280014c78fbcf9a003300e5f654bf7
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-01 21:08:34 -0400

    se creo la base de datos

---
+++ app/src/main/java/edu/ucne/ronalfyjimenez_ap2_p1/data/local/database/HuacalesDb.kt
@@ -0,0 +1,15 @@
+package edu.ucne.ronalfyjimenez_ap2_p1.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.ronalfyjimenez_ap2_p1.data.local.dao.HuacalDao
+import edu.ucne.ronalfyjimenez_ap2_p1.data.local.entity.HuacalEntity
+
+@Database(
+    entities = [HuacalEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class HuacalesDb : RoomDatabase() {
+    abstract fun huacalDao(): HuacalDao
+}
